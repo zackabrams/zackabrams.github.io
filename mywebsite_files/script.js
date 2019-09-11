426c1bcd6909b550f421916ab0534e6698a47ced
Zack Abrams <zackabrams@dyn-160-39-141-123.dyn.columbia.edu>
2019-09-11 18:06:41 -0400

    Final commit (for now)

--- mywebsite_files/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('a[href*=#letters]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
